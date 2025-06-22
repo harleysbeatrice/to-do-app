a307424280ebcb7fa0a8c5f7ba215552db462227
harleysbeatrice <deveci.umut@gmail.com>
2025-06-22 11:47:00 +0300

    functionality added

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 const commitButton = document.querySelector("#commit");
 
+function delItem (button) {
+    let listItem = button.parentNode;
+    listItem.remove()
+}
+
+const doneList = document.querySelector(".done > ul");
+const undoneList = document.querySelector(".undone > ul");
+
+function checkTask(box) {
+    let listItem = box.parentNode;
+    if (box.checked) {
+        doneList.appendChild(listItem);
+    } else {
+        undoneList.appendChild(listItem);
+    }
+}
+
 function commitTask () {
     const textInput = document.querySelector("#task-input");
 
-    let task = textInput.textContent;
+    let task = textInput.value;
 
-    let newItem = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("class", "check");
-    newItem.appendChild(checkbox);
-    newItem.textContent = task;
+    if (task) {
+        let newItem = document.createElement("li");
 
-    const delButton = document.createElement("button");
-    delButton.setAttribute("class", "del");
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("class", "check");
+        checkbox.addEventListener("change",() => checkTask(checkbox));
+        newItem.appendChild(checkbox);
 
-    const trashIcon = document.createElement("img");
-    trashIcon.src = "trash.svg";
+        const taskText = document.createElement("p");
+        taskText.textContent = task;
+        newItem.appendChild(taskText);
 
-    delButton.appendChild(trashIcon);
+        const delButton = document.createElement("button");
+        delButton.setAttribute("class", "del");
+        delButton.addEventListener("click", () => delItem(delButton))
 
-    newItem.appendChild(delButton);
+        const trashIcon = document.createElement("img");
+        trashIcon.src = "trash.svg";
 
-    const undoneList = document.querySelector(".undone > ul");
-    undoneList.appendChild(newItem);
+        delButton.appendChild(trashIcon);
 
+        newItem.appendChild(delButton);
 
+        const undoneList = document.querySelector(".undone > ul");
+        undoneList.appendChild(newItem);
+
+        textInput.value = null;
+    } else {
+        return
+    }
 }
 
-
 commitButton.addEventListener("click", commitTask);
+
+
+
+
+
